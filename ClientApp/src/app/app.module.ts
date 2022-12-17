c9c5aef2d29a805a9d05770096eae435af9c0faf
mgarnick1 <markgman008@gmail.com>
2022-12-17 14:20:35 -0700

    added a loading spinner with http interceptor

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -10,21 +10,31 @@ import { HomeComponent } from './home/home.component';
 import { ErrorHandlerService } from 'src/shared/services/error-handler.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {ReactiveFormsModule} from '@angular/forms';
-import { MatInputModule} from '@angular/material/input'
-import {MatIconModule} from '@angular/material/icon'
-import {MatCardModule} from '@angular/material/card'
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AddTvSeriesComponent } from './home/add-tv-series/add-tv-series.component';
 import { TvSeriesCardComponent } from './home/tv-series-card/tv-series-card.component';
 import { TvRatingStarComponent } from './home/tv-rating-star/tv-rating-star.component';
+import { SpinnerComponent } from './home/spinner/spinner.component';
+import { LoadingInterceptor } from 'src/shared/services/loading.interceptor';
 
 @NgModule({
-  declarations: [AppComponent, NavMenuComponent, HomeComponent, AddTvSeriesComponent, TvSeriesCardComponent, TvRatingStarComponent],
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    AddTvSeriesComponent,
+    TvSeriesCardComponent,
+    TvRatingStarComponent,
+    SpinnerComponent,
+  ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
@@ -49,12 +59,17 @@ import { TvRatingStarComponent } from './home/tv-rating-star/tv-rating-star.comp
     MatIconModule,
     MatFormFieldModule,
     MatCardModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
   ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ErrorHandlerService,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingInterceptor,
       multi: true,
     },
   ],
